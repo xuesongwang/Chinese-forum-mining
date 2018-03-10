63622049c8eac8e4e29a6366f68c11ff0c2cf4c9
xuesong <673046307@qq.com>
2018-03-10 19:14:23 +0800

    修改Binary SVM 文件

--- SVM/BinaryclassSVM.py
+++ SVM/BinaryclassSVM.py
@@ -35,39 +35,37 @@ def Readfile(validname,rubbishname,size,rubbishsize):
     return x,y
 
 
-def FeaureExtraction(contents,vocab_processor = None):
-    """ pre-processing input data ,removing meaningless signs"""
-    seg_list = []
-    coder = "[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）“”]+"
-    for content in contents:
-        content = str(content).encode("utf-8")
-        string = re.split(coder.decode("utf8"),content)
-        for substr in string :
-            if substr:
-                s = filter(str.isdigit, substr)
-                if s:
-                    seg_list.append(copy.deepcopy(s))
-                seg_list.append(substr.replace(s,''))
-    """ remove redundant records """
-    seg_list = list(set(seg_list))
-    contentlist =[]
-    for l in seg_list:
-        l = str(l).encode("utf-8")
-        word = list(jb.cut(l))
-        seg = ''
-        for x in word:
-            seg += x.strip() + ' '
-        contentlist.append(seg)
-    """ pad each sentence to the same length and map each word to an id"""
-    if vocab_processor:
-        x = np.array(list(vocab_processor.transform(contentlist)))
-    else:
-        max_document_length = max([len(seg.split(' ')) for seg in contentlist])
+def wordSeg(contents,initial_vocab=None,vocab_processor=None):
+    """first time just use vocab_recordsize to decide how many words to build a vocabulary """
+    if initial_vocab:
+        seg_list =[]
+        for content in contents[0:initial_vocab]:
+            content = str(content).encode("utf-8")
+            word = list(jb.cut(content))
+            seg = ''
+            # split words and catch them with whitespace
+            for x in word:
+                seg += x.strip() + ' '
+            seg_list.append(seg)
+        """pad each sentence to the same length and map each word to an id"""
+        max_document_length = max([len(seg.split(' ')) for seg in seg_list])
         logging.info('The maximum length of all sentences in training set: {}'.format(max_document_length))
         vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
         x = np.array(list(vocab_processor.fit_transform(seg_list)))
-    return x,vocab_processor,seg_list
+        # vocab_processor.save(os.path.join("../CNN/data/vocab.pickle"))
 
+    # """only need to cut and transform testing set"""
+    else:
+        seg_list =[]
+        for content in contents:
+            content = str(content).encode("utf-8")
+            word = list(jb.cut(content))
+            seg = ''
+            for x in word:
+                seg += x.strip() + ' '
+            seg_list.append(seg)
+        x = np.array(list(vocab_processor.transform(seg_list)))
+    return x, vocab_processor
 
 def Predict(alldata):
     """predict whether alldata is valid """
@@ -83,15 +81,14 @@ if __name__ == '__main__':
     x,y =Readfile('../DataSource/valid.csv','../DataSource/rubbish.csv',4000,3000)
     x_train, x_test, y_, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
     # a = Predict(x_train)
-    x_, vocab_processor = FeaureExtraction(x_train)
-    x_test,vocab_processor = FeaureExtraction(x_test,vocab_processor)
+    x_train_encode, vocab_processor = wordSeg(x_train,initial_vocab=x_train.shape[0])
+    x_test_encode,vocab_processor = wordSeg(x_test,vocab_processor=vocab_processor)
     clf = svm.SVC()
-    clf.fit(x_, y_)
-    print clf.score(x_test,y_test)
-    cm = confusion_matrix(y_test,clf.predict(x_test),labels=['rubbish','valid'])
+    clf.fit(x_train_encode, y_)
+    print clf.score(x_test_encode,y_test)
+    cm = confusion_matrix(y_test,clf.predict(x_test_encode),labels=['rubbish','valid'])
     print cm
     with open('../SVM/data/binary_vocab.pkl', 'w') as f:  # open file with write-mode
         pickle.dump(vocab_processor, f)
     with open('../SVM/binarymodel.pkl', 'w') as f:
         joblib.dump(clf, f)
-
